7de8dd91c112a8414965933481dc5a2e36790062
Tony Zhang <tzz3@cornell.edu>
2020-06-04 05:55:40 -0400

    ID page properly displaying information

--- src/Components/Identification.jsx
+++ src/Components/Identification.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {useState} from 'react';
 import Youtube from 'react-youtube';
+import {Card, CardContent, Typography} from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
 export default (props) => {
     const [temp, setTemp] = useState(
@@ -9,9 +11,9 @@ export default (props) => {
             status: "Finished",
             embed: "https://www.youtube.com/watch?v=z2LqC2y5JCY",
             clipper: "Rushi",
-            clip_time: new Date('December 17, 1995 03:24:00'),
+            clipper_time: new Date('December 17, 1995 03:24:00'),
             translator: "Ren",
-            translate_time: new Date('December 17, 1995 03:24:00'),
+            translator_time: new Date('December 17, 1995 03:24:00'),
             proofreader: "MoonPotatoes",
             proofreader_time: new Date('December 17, 1995 03:24:00'),
             subber: "Blue",
@@ -27,12 +29,14 @@ export default (props) => {
         }
     );
 
+    const [roles, ___] = useState(["clipper", "translator", "proofreader", "subber", "editor", "thumbnailer", "QA"]);
+
     const opts = {
         height: '390',
         width: '640',
         playerVars: {
           // https://developers.google.com/youtube/player_parameters
-          autoplay: 1,
+          autoplay: 0,
         },
       };
       
@@ -42,8 +46,31 @@ export default (props) => {
             <div style = {{ margin: '0 auto', fontSize: 72}}>ID-{temp.id}</div>
             <div style = {{ margin: '0 auto', fontSize: 36}}>Status: {temp.status}</div>
             <Youtube videoId = "z2LqC2y5JCY" opts = {opts} ></Youtube>
+            <div id = "contributors" style={{margin: '0 auto', fontSize: 36}}>
+                {roles.map(
+                    (roles) => {
+                       return <UserTag role = {roles} user = {temp[roles]} time = {temp[roles+"_time"]}/>
+                    }
+                )}
+            </div>
         </div>
     
     );
 }
 
+
+
+class UserTag extends React.Component {
+   
+
+    render(){
+        return <Card variant = "outlined">
+            <CardContent>
+            <Typography className="title" color="textSecondary" gutterBottom>{this.props.role}</Typography>
+            <Typography variant="h5" component="h2">{this.props.user}</Typography>
+            <Typography className="test" color="textSecondary">{this.props.time.toString()}</Typography>
+            </CardContent>
+        </Card>;
+    }
+
+}
