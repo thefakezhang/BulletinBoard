eebdc23e44bfb3a2889b0f83b308da3f699c779f
Tony Zhang <tzz3@cornell.edu>
2020-06-07 09:34:50 -0400

    starting work on user page

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import {useState} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {Card, CardContent, Typography} from '@material-ui/core';
+
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+  });
+  
 
 export default (props) => {
+    //All the job types that a user can take
+    const [roles, editRoles] = useState(["Translator", "proofreader"]);
+
+    //Past works of this user
+    const [pastJobID, editPastJobID] = useState([1,4,6,8,23,45,68,69])
+    const [pastJobType, editPastJobType] = useState(["proofreading", "translating", "translating", "proofreading","proofreading", "translating", "translating", "proofreading"]);
+    
+    //Current works of this user
+    const [currentJobID, editCurrentJobID] = useState([71, 73]);
+    const [currentJobType, editCurrentJobType] = useState(["proofreading", "translating"]);
+
+    const classes = useStyles();
     return(
-    props.match.params.username === ""? 
-    <p>Try adding in a username param</p>
-    :
-    <p>this is {props.match.params.username}'s user page</p>
+        <div>
+            <p>this is {props.match.params.username}'s user page</p>
+            <div id="tags">
+                Roles:{"\t"} 
+                {roles.map(
+                    (roleName) => {
+                        return roleName + " "
+                    }
+                )}
+            </div>
+
+            <p>Currently working on: </p>
+
+        </div>
+    
     
     )
 }
